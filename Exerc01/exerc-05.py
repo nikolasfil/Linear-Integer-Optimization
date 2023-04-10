e0fb917ca90b9023b2871150cb5d984507f15987
nikolasfil <nikolas13in@gmail.com>
2023-04-10 11:41:18 +0300

    ask5 part a , beautify

--- Exerc01/exerc-05.py
+++ Exerc01/exerc-05.py
@@ -1,6 +1,11 @@
 import numpy as np
 from itertools import combinations
 
+def presenting(li):
+    return  f"{str(li[0] if li[0]!=1 else '')+'x1' if li[0]!=0 else '' }"+f"{'+' if li[1]>0 else ''}" +\
+            f"{str(li[1] if li[1]!=1 else '')+'x2' if li[1]!=0 else '' }"+f"{'+' if li[2]>0 else ''}" +\
+            f"{str(li[2] if li[2]!=1 else '')+'x3' if li[2]!=0 else '' }" + f"={li[3]}"
+
 def main():
     l = [   [-3,2,8,17],
             [-1,1,3,9],
@@ -28,7 +33,8 @@ def main():
         result = np.linalg.solve(a,b)
         if np.allclose(np.dot(a, result), b):
            results.append(result)
-        print(f'{str(value[0]):14s} \n'+f'{str(value[1]):14s} = {result} \n'+f'{str(value[2]):14s} \n')
+        # print(f'{str(value[0]):14s} \n'+f'{str(value[1]):14s} = {result} \n'+f'{str(value[2]):14s} \n')
+        print('\n'.join([f'{presenting(value[0]):14s} ',f'{presenting(value[1]):14s} => {result} ',f'{presenting(value[2]):14s} \n']))
 
     print(len(results))
 
