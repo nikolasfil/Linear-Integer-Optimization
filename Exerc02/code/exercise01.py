e7782ec7b2715c59ef61d39008f9bc3e4b161155
nikolasfil <nikolas13in@gmail.com>
2023-05-31 20:59:28 +0300

    making it more usable

--- Exerc02/code/exercise01.py
+++ Exerc02/code/exercise01.py
@@ -48,15 +48,15 @@ def main_universal():
 
 
 
-def main_a(x,c,obj):
-    model = pulp.LpProblem(name="assignment1a", sense=pulp.LpMaximize)
+def main_a(x,c,obj,name,sense):
+    model = pulp.LpProblem(name=name, sense=sense)
 
     for i in range(len(c)):
         model += mul(x,c[i],'laq')
     model += mul(x,obj)
 
     solver = pulp.GLPK_CMD(msg=0)
-    status = model.solve(solver)
+    model.solve(solver)
 
     print('-'*50)
     print(f"status: {model.status}, {pulp.LpStatus[model.status]}")
@@ -69,32 +69,6 @@ def main_a(x,c,obj):
         print(f"""{x}: {constraint.value()}""")
     print('-'*50)
 
-
-def main():
-    model = pulp.LpProblem(name="assignment1a", sense=pulp.LpMaximize)
-
-    x = [pulp.LpVariable(name=f"x{i+1}", lowBound=0) for i in range(4)]
-    c = [[1, 3, 0, 1, 8], [2, 1, 0, 0, 6], [0, 2, 4, 1, 6]]
-    obj = [2, 4, 1, 1]
-
-
-    for i in range(len(c)):
-        model += mul(x,c[i],'laq')
-    model += mul(x,obj)
-
-    solver = pulp.GLPK_CMD(msg=0)
-    status = model.solve(solver)
-
-    print('-'*50)
-    print(f"status: {model.status}, {pulp.LpStatus[model.status]}")
-    print(f"objective: {model.objective.value()}")
-    print('-'*50)
-    for var in x:
-        print(f"{var.name}: {var.value()}")
-    print('-'*50)
-    for x,constraint in model.constraints.items():
-        print(f"""{x}: {constraint.value()}""")
-    print('-'*50)
 
 
 
@@ -102,7 +76,13 @@ def main_b():
     model = pulp.LpProblem(name='assignment1b',sense=pulp.LpMaximize)
 
 
+def main():
 
+    x = [pulp.LpVariable(name=f"x{i+1}", lowBound=0) for i in range(4)]
+    c = [[1, 3, 0, 1, 8], [2, 1, 0, 0, 6], [0, 2, 4, 1, 6]]
+    obj = [2, 4, 1, 1]
+
+    main_a(x,c,obj,"assignment1a",pulp.LpMaximize)
 
 
 if __name__ == "__main__":
