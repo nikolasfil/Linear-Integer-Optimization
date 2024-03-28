af7f702b3ad5be1094264f4aedcd3e69b0059b7f
nikolasfil <nikolas13in@gmail.com>
2024-03-28 17:13:45 +0200

    checkpoint update

--- 2023-2024/Assignment_1/exercise_01.py
+++ 2023-2024/Assignment_1/exercise_01.py
@@ -23,16 +23,47 @@ class Gui(LinearGUI):
         # self.lines.append(line(10, -1, -10, "l2", type="line"))
         self.lines.append(line(-4, 1, 20, "l3", type="line"))
         self.lines.append(line(1, 4, 20, "l4", type="line"))
+        # self.lines.append(line(11, -1, (11 * 1.67 - 26.67), "l7", type="line"))
+        # self.lines.append(line(11, -1, (-8.5), "l8", type="line"))
+
+        # self.lines.append(line(2, -1, -23.3, "l5", type="line"))
+        # self.lines.append(line(2, -1, -24, "l6", type="line"))
 
     def find_feasible_points(self):
-        self.v.append([0, 10])
-        self.plt.plot(*self.v[0], "o", color="red")
+
+        # self.v.append([0, 10])
+        # self.plt.plot(*self.v[-1], "o", color="red")
+
+        # Intersection lines 1 and 2
+        self.v.append(self.lines[0].intersection(self.lines[1]))
+
+        # Intersection lines 2 and 3
         self.v.append(self.lines[2].intersection(self.lines[1]))
-        self.v.append([0, 20])
-        self.plt.plot(*self.v[2], "o", color="red")
+
+        # Upper left limit corner
+        self.v.append([self.lines[2](self.limit, reverse=True), self.limit])
+        # self.plt.plot(*self.v[-1], "o", color="red")
+
+        # Upper right corner
+        self.v.append([self.limit, self.limit])
+        # self.plt.plot(*self.v[-1], "o", color="red")
+
+        # Lower right corner
+        self.v.append([self.limit, 0])
+        # self.plt.plot(*self.v[-1], "o", color="red")
+
+        # Intersection line 3 and line 2
+        # # self.v.append([self.lines[3](0, reverse=True), 0])
+        # self.plt.plot(*self.v[-1], "o", color="red")
+        self.v.append(self.lines[3].intersection(self.lines[2]))
+
+        # Intersection line 4 and line 1
+        self.v.append(self.lines[0].intersection(self.lines[3]))
+        # self.plt.plot(*self.v[-1], "o", color="red")
+
+        # self.plt.plot(*self.v[-1], "o", color="red")
 
     def standard_feasible_points(self):
-
         pass
 
     def solution(self, a, b, minlim, maxlim, legend=True):
@@ -63,7 +94,7 @@ class Gui(LinearGUI):
 def main():
 
     gui = Gui(plt, name="Feasible Region")
-    # gui.create_figure()
+    gui.create_figure()
     # gui.plt.savefig("img/exerc01_a.png", dpi="figure")
 
     # gui.name = "Z: min 2x1-x2"
@@ -71,10 +102,10 @@ def main():
     # gui.solution(2, -1, -12, -9)
     # gui.plt.savefig("img/exerc01_a_2.png", dpi="figure")
 
-    gui.name = "Z: min 11x1-x2"
-    gui.create_figure()
-    gui.solution(11, -1, -13, -8)
-    gui.plt.savefig("img/exerc01_b.png", dpi="figure")
+    # gui.name = "Z: min 11x1-x2"
+    # gui.create_figure()
+    # gui.solution(11, -1, -13, -8)
+    # gui.plt.savefig("img/exerc01_b.png", dpi="figure")
 
     gui.show()
 
