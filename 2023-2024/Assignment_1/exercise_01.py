ba7cbe0b6951e43fb993157d7823a8b5426fe704
nikolasfil <nikolas13in@gmail.com>
2024-04-06 22:09:06 +0300

    checkpoint

--- 2023-2024/Assignment_1/exercise_01.py
+++ 2023-2024/Assignment_1/exercise_01.py
@@ -14,21 +14,12 @@ class Gui(LinearGUI):
         limit = 30
 
         kwargs["limit"] = limit
-        # kwargs["ylim"] = [-5, limit]
-        # kwargs["xlim"] = [-5, limit]
         super().__init__(*args, **kwargs)
         self.lines = []
         self.lines.append(line(1, 1, 10, "l1", type="line"))
         self.lines.append(line(-10, 1, 10, "l2", type="line"))
-        # self.lines.append(line(10, -1, -10, "l2", type="line"))
         self.lines.append(line(-4, 1, 20, "l3", type="line"))
         self.lines.append(line(1, 4, 20, "l4", type="line"))
-
-        # self.lines.append(line(11, -1, (11 * 1.67 - 26.67), "l7", type="line"))
-        # self.lines.append(line(11, -1, (-8.5), "l8", type="line"))
-
-        # self.lines.append(line(2, -1, -23.3, "l5", type="line"))
-        # self.lines.append(line(2, -1, -24, "l6", type="line"))
 
     def find_feasible_points(self):
 
@@ -69,28 +60,10 @@ class Gui(LinearGUI):
     def standard_feasible_points(self):
         pass
 
-    def solution(self, a, b, minlim, maxlim, legend=True):
-
-        # line_l3 = [l for l in self.lines if l.name == "l3"][0]
-        # line_l1 = [l for l in self.lines if l.name == "l1"][0]
-        # extra_lines = [line(a, b, i, "extra") for i in range(minlim, maxlim, step=0.5)]
-
-        counter = minlim
-        while counter < maxlim:
-            extra = line(a, b, counter, "extra")
-            extra.plot(self.xAxis, "cornflowerblue")
-            extra.legend_show = False
-            for l in self.lines:
-                possible = extra.intersection(l, plotting=False)
-                if possible and self.check_feasible_point(possible):
-                    extra.intersection(l)
-            counter += self.step
-
-
-class Exercise01:
-    def __init__(self, *args, **kwargs) -> None:
-        self.gui = kwargs.get("gui", Gui(plt, name="Feasible Region", figsize=(15, 8)))
-        self.save_images = kwargs.get("save_images", False)
+    # class Exercise01:
+    #     def __init__(self, *args, **kwargs) -> None:
+    #         self.gui = kwargs.get("gui", Gui(plt, name="Feasible Region", figsize=(15, 8)))
+    #         self.save_images = kwargs.get("save_images", False)
 
     def save_image(self, file_name):
         if self.save_images:
@@ -107,14 +80,15 @@ class Exercise01:
     def exerc01_a(self):
         self.gui.name = "Z: min 2x1-x2"
         self.gui.create_figure()
-        self.gui.solution(2, -1, -25, 5)
+        self.gui.step = 5
+        self.gui.graphical_solution(2, -1, -25, 5)
         self.save_image("exerc01_a_1.png")
 
     def exerc01_b(self):
         self.gui.name = "Z: min 11x1-x2"
         self.gui.create_figure()
         self.gui.step = 5
-        self.gui.solution(11, -1, -15, 5)
+        self.gui.graphical_solution(11, -1, -15, 5)
         self.save_image("exerc01_b.png")
 
     def exerc01_c(self):
@@ -141,12 +115,18 @@ class Exercise01:
 def main():
     gui = Gui(plt, name="Feasible Region", figsize=(15, 8))
 
-    exerc01 = Exercise01(gui=gui, save_images=True)
-    exerc01.save_images = False
+    # exerc01 = Exercise01(gui=gui, save_images=True)
+    # exerc01.save_images = False
     # exerc01.feasible_region()
     # exerc01.exerc01_a()
-    exerc01.exerc01_b()
+    # exerc01.exerc01_b()
     # exerc01.exerc01_c()
+
+    gui.save_images = False
+    gui.feasible_region()
+    gui.exerc01_a()
+    gui.exerc01_b()
+    gui.exerc01_c()
 
     gui.show()
 
