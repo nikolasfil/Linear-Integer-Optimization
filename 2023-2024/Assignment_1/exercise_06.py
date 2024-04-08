f0c1e4437e2e3c56db39d473d378cd533f56b387
nikolasfil <nikolas13in@gmail.com>
2024-04-08 12:19:46 +0300

    updated the simplex problem

--- 2023-2024/Assignment_1/exercise_06.py
+++ 2023-2024/Assignment_1/exercise_06.py
@@ -8,13 +8,27 @@ from Tools.simplex_solver import SimplexSolver
 
 
 class Exerc06(SimplexSolver):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.parent = Path(__file__).parent
 
     def main(self):
         A = np.array([[1, 2, -3, 1], [3, 3, 1, 3]])
         b = np.array([24, 36])
         c = np.array([5, 4, -1, 3])
 
+        # A = np.array(
+        #     [
+        #         [1, 2, 3, 1, 0, 0],
+        #         [3, 1, 2, 0, 1, 0],
+        #         [2, 3, 1, 0, 0, 1],
+        #     ]
+        # )
+        # b = np.array([2, 2, 4])
+        # c = np.array([-1, -4, -5, 0, 0, 0])
+
         simplex = SimplexSolver(A=A, b=b, c=c)
+        simplex.parent = self.parent
         simplex.solve()
 
 
