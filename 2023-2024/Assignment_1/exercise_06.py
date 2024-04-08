2936d2be496d2a213594bc9f97a5e77695599396
nikolasfil <nikolas13in@gmail.com>
2024-04-08 11:58:04 +0300

    checkpoint

--- 2023-2024/Assignment_1/exercise_06.py
+++ 2023-2024/Assignment_1/exercise_06.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 import sys
 from pathlib import Path
+import numpy as np
 
 sys.path.append(str(Path(__file__).parents[2]))
+from Tools.simplex_solver import SimplexSolver
 
 
-class Exerc06:
-
-    def __init__(self) -> None:
-        pass
+class Exerc06(SimplexSolver):
 
     def main(self):
-        pass
+        A = np.array([[1, 2, -3, 1], [3, 3, 1, 3]])
+        b = np.array([24, 36])
+        c = np.array([5, 4, -1, 3])
+
+        simplex = SimplexSolver(A=A, b=b, c=c)
+        simplex.solve()
 
 
 if __name__ == "__main__":
+    exercise = Exerc06()
+    exercise.main()
     try:
         pass
     except KeyboardInterrupt:
