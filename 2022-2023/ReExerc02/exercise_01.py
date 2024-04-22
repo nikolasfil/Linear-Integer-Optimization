2a7ec9c9361d44b256e9f305f2228c367b399866
nikolasfil <nikolas13in@gmail.com>
2024-04-22 07:40:32 +0300

    checkpoint

--- 2022-2023/ReExerc02/exercise_01.py
+++ 2022-2023/ReExerc02/exercise_01.py
@@ -9,6 +9,30 @@ class Exerc01(PulpSolver):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
+    def exerc_01_a(self):
+        self.b = [
+            [1, 3, 0, 1],
+            [2, 1, 0, 0],
+            [0, 1, 4, 1],
+            [2, 4, 1, 1],
+        ]
+        self.b = [
+            [1, 3, 0, 1],
+            [-2, -1, 0, 0],
+            [0, 1, 4, 1],
+            [2, 4, 1, 1],
+        ]
+        self.c = [8, -6, 6, 0]
+        # can be written better
+        self.inequality = ["<=", "<=", "<=", "<="]
+        self.obj = [2, 4, 1, 1]
+        self.sense = "max"
+        self.create_components()
+
+    def main(self):
+        self.exerc_01_a()
+
 
 if __name__ == "__main__":
-    exerc = Exerc01(3, "x", lowbound=0)
+    exerc = Exerc01(name="Exercise1a")
+    exerc.main()
--- Tools/pulp_solver.py
+++ Tools/pulp_solver.py
@@ -21,14 +21,18 @@ class PulpSolver:
                 The b vector of the model. Default is [].
             c: list
                 The c vector of the model. Default is [].
-
+            obj: list
+                The objective function of the model. Default is [].
+            inequality: list
+                The inequality of the model. Default is [].
         """
         self.name = kwargs.get("name", "PulpSolver")
         self.sense = kwargs.get("sense", None)
-        self.sensing()
 
         self.b = kwargs.get("b", [])
         self.c = kwargs.get("c", [])
+        self.obj = kwargs.get("obj", [])
+        self.inequality = kwargs.get("inequality", [])
 
     def sensing(self):
         if self.sense == "max":
@@ -41,11 +45,37 @@ class PulpSolver:
             raise ValueError("Sense must be either 'max' or 'min'.")
 
     def create_components(self):
+        print(self.b, self.c, self.obj, self.inequality)
+        self.sensing()
+
         self.model = pulp.LpProblem(self.name, sense=self.sense)
         self.build_model()
-        self.solver = pulp.GLKP_CMD()
+        self.solver = pulp.GLPK_CMD()
 
         self.status = self.model.solve(self.solver)
+
+    def build_model(self):
+        """
+        Description:
+        """
+
+        # Takes the number of variables and creates them.
+        self.x = [
+            pulp.LpVariable(name=f"x{i}", lowBound=0)
+            for i in range(1, len(self.b[0]) + 1)
+        ]
+
+        for i, row in enumerate(self.b):
+
+            temp = "".join(
+                [str(row[j]) + " * " + self.x[j].name + " + " for j in range(len(row))]
+            )
+
+            self.model += (
+                sum([self.x[j] * row[j] for j in range(len(row))]) <= self.c[i]
+            )
+
+        self.model += sum([self.x[i] * self.obj[i] for i in range(len(self.obj))])
 
 
 if __name__ == "__main__":
